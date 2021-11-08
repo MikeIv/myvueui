7e3a6fd61eb69bad3c436eb36bcb61c7cd6b38d1
MIUIvanov <miuivanov@synergy.ru>
2021-11-08 13:27:02 +0300

    add prj

---
+++ src/components/m_test/m_test.vue
@@ -0,0 +1,15 @@
+<template>
+  <p class="m_test__p">Тест</p>
+</template>
+
+<script>
+import './m_test.scss'
+
+export default {
+  name: "m_test"
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <MTest />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import MTest from '@/components/m_test/m_test'
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    MTest,
   },
 };
 </script>
